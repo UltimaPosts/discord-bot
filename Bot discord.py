05f9470920178723fc9ea6c3d796849c0e4840c9
UltimaPosts <gingalevi@gmail.com>
2022-06-10 15:41:16 +0200

    Create Bot discord.py

---
+++ Bot discord.py
@@ -0,0 +1,170 @@
+import discord
+from discord.ext import commands, tasks
+from discord import Member
+from discord.ext.commands import has_permissions, MissingPermissions
+from discord.utils import get
+import youtube_dl
+import random
+from dotenv import load_dotenv
+import youtube_dl
+import asyncio
+import aiohttp
+
+
+load_dotenv()
+
+intents = discord.Intents.default()
+intents.members = True
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+
+
+
+
+
+
+
+
+
+
+
+
+youtube_dl.utils.bug_reports_message = lambda: ''
+ytdl_format_options = {
+    'format': 'bestaudio/best',
+    'restrictfilenames': True,
+    'noplaylist': True,
+    'nocheckcertificate': True,
+    'ignoreerrors': False,
+    'logtostderr': False,
+    'quiet': True,
+    'no_warnings': True,
+    'default_search': 'auto',
+    'source_address': '0.0.0.0' # bind to ipv4 since ipv6 addresses cause issues sometimes
+}
+ffmpeg_options = {
+    'options': '-vn'
+}
+ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
+class YTDLSource(discord.PCMVolumeTransformer):
+    def __init__(self, source, *, data, volume=0.5):
+        super().__init__(source, volume)
+        self.data = data
+        self.title = data.get('title')
+        self.url = ""
+    @classmethod
+    async def from_url(cls, url, *, loop=None, stream=False):
+        loop = loop or asyncio.get_event_loop()
+        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
+        if 'entries' in data:
+            # take first item from a playlist
+            data = data['entries'][0]
+        filename = data['title'] if stream else ytdl.prepare_filename(data)
+        return filename
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+@bot.command()
+async def hello(ctx):
+    await ctx.reply('Hello!')                       #sends hello to the person who used the command
+        
+
+
+@bot.command()
+async def addrole(ctx, member : discord.Member, role : discord.Role):
+    await member.add_roles(role)
+
+
+
+
+@bot.command()
+async def join(ctx):
+    channel = ctx.author.voice.channel
+    await channel.connect()
+
+
+@bot.command()
+async def play(ctx,url):
+    try :
+        server = ctx.message.guild
+        voice_channel = server.voice_client
+        async with ctx.typing():
+            filename = await YTDLSource.from_url(url, loop=bot.loop)
+            voice_channel.play(discord.FFmpegPCMAudio(executable="ffmpeg.exe", source=filename))
+        await ctx.send('**Now playing:** {}'.format(filename))
+    except:
+        await ctx.send("The bot is not connected to a voice channel.")
+@bot.command()
+async def pause(ctx):
+    voice_client = ctx.message.guild.voice_client
+    if voice_client.is_playing():
+        await voice_client.pause()
+    else:
+        await ctx.send("The bot is not playing anything at the moment.")
+    
+@bot.command()
+async def resume(ctx):
+    voice_client = ctx.message.guild.voice_client
+    if voice_client.is_paused():
+        await voice_client.resume()
+    else:
+        await ctx.send("The bot was not playing anything before this. Use play_song command")
+@bot.command()
+async def stop(ctx):
+    voice_client = ctx.message.guild.voice_client
+    if voice_client.is_playing():
+        await voice_client.stop()
+    else:
+        await ctx.send("The bot is not playing anything at the moment.")
+
+
+
+
+
+@bot.command()
+async def leave(ctx):
+    if (ctx.voice_client):                          #if he is in the voice channel.
+        await ctx.voice_client.disconnect()
+        await ctx.send('I left :)')
+    else:                                           #if he is not in the voice channel.
+        await ctx.send("I'm not in the voice channel. Use the music or join command to make me join.")
+
+
+
+
+@bot.event
+async def on_member_join(member):
+    await member.send('Private message')
+
+
+
+
+
+@bot.command(pass_context=True)
+async def meme(ctx):
+    embed = discord.Embed(title="", description="")
+
+    async with aiohttp.ClientSession() as cs:
+        async with cs.get('https://www.reddit.com/r/dankmemes/new.json?sort=hot') as r:
+            res = await r.json()
+            embed.set_image(url=res['data']['children'] [random.randint(0, 25)]['data']['url'])
+            await ctx.send(embed=embed)
+
+
+
+
+
+
+bot.run('OTg0NDc4Nzk1Mjk5OTU4ODE0.GQdwPn.P9RFo5oBkEHzkI8hx0sxzrVR70-b7XzAH08E0c')
